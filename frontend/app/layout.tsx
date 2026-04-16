7956c2e7c9b422026ba23ce79f00023575885c12
Bristi Maharjan <bristimah@gmail.com>
2026-04-16 22:23:09 +0545

    metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "License Plate Detection Inference",
-  description: "Upload an image and run backend YOLO detection",
+  description: "Upload an image and run backend YOLO detection CNN model to detect license plates.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
